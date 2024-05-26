cb8b57bbc9e77ba7fac393a2cdc98faf4065c747
wuming <132824538+name1908@users.noreply.github.com>
2024-05-26 16:26:31 +0800

    Update ttsc.js

--- ttsc.js
+++ ttsc.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目名称：天天素材
+项目名称：ds
 下载地址：
 脚本作者：
 电报频道：
@@ -9,18 +9,70 @@
 **************************************
 
 [rewrite_local]
-^http://108.181.40.58:88/api/v2/saveiaporder/8?token=319e4da71716307468365bnge&device=2.3,5.18,iPhone%2014%20Pro%20Max,16.5,zh-Hans&uid=dd57d3e88&v=562 url script-response-body https://raw.githubusercontent.com/name1908/Rewrite/main/ttsc.js   
+^https://fast.lmfasting.cn/api/Order/ConfigList url script-response-body https://raw.githubusercontent.com/name1908/Rewrite/main/ttsc.js   
 
 
 [mitm]
-hostname = 108.181.40.58:88
+hostname = fast.lmfasting.cn
 
 **/
-var objc = JSON.parse($response.body);
-objc={
-  {
-  "status" : "1",
-  "data" : "123484"
+var name1908 = JSON.parse($response.body);
+objc name1908={
+ {
+	"code" : 200,
+	"data" :  [
+		 {
+			"count" : 36000,
+			"discount" : 2,
+			"flag" : "permanent",
+			"freeCount" : 0,
+			"hidden" : true,
+			"intro" : "一次付费，所有功能无限次使用",
+			"isRecommend" : true,
+			"isSub" : true,
+			"orderType" : 1,
+			"platform" : 0,
+			"price" : 0,
+			"primePrice" : 0,
+			"productId" : "p_vip_0",
+			"text" : "永久会员"
+		},
+		 {
+			"count" : 30,
+			"discount" : 5,
+			"flag" : "month",
+			"freeCount" : 0,
+			"hidden" : false,
+			"intro" : "平均每日1.6",
+			"isRecommend" : false,
+			"isSub" : true,
+			"orderType" : 1,
+			"platform" : 0,
+			"price" : 0,
+			"primePrice" : 0,
+			"productId" : "vip_subscribe_months_48",
+			"text" : "月卡会员"
+		},
+		 {
+			"count" : 360,
+			"discount" : 3,
+			"flag" : "year",
+			"freeCount" : 0,
+			"hidden" : false,
+			"intro" : "平均每日0.27",
+			"isRecommend" : true,
+			"isSub" : true,
+			"orderType" : 1,
+			"platform" : 0,
+			"price" : 0,
+			"primePrice" : 0,
+			"productId" : "vip_subscribe_years_0",
+			"text" : "年卡会员"
+		}
+	],
+	"msg" : "ok",
+	"t" : "2024-05-26 15:47:42"
 }
+
 
 $done({body : JSON.stringify(objc)});
