dce99ba6b2a176022336479fb701d34f08e7dceb
wuming <132824538+name1908@users.noreply.github.com>
2024-05-31 14:19:07 +0800

    Update gs.js

--- gs.js
+++ gs.js
@@ -25,8 +25,8 @@ name1908={
 	"msg" : "初始值",
 	"status" : true,
 	"statusCode" : 200,
-	"svip" : "/true(946656000000)/",
-	"vip" : "/true(946656000000)/"
+	"svip" : "/2099-09-09（946656000000)/",
+	"vip" : "/2099-09-09（946656000000)/"
 }
 
 $done({body : JSON.stringify(name1908)});
