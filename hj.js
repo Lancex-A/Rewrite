f38b98729104c4f1d7fb81fa99f75a0121e35a6f
wuming <132824538+Lancex-A@users.noreply.github.com>
2024-06-10 19:06:32 +0800

    Update hj.js

--- hj.js
+++ hj.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目名称：
+项目名称：hj
 下载地址：
 脚本作者：Lancex-A
 电报频道：
@@ -8,14 +8,17 @@
 
 *************************************
 [rewrite_local]
-^https://api.qianmoai.com/api/vip/v1/my url script-response-body
+^https://api.qianmoai.com/api/vip/v1/my url script-response-body https://raw.githubusercontent.com/Lancex-A/Rewrite/main/hj.js
 [mitm]
-hostname = 
+hostname = api.qianmoai.com
 *************************************
 var Lancex-A = $response.body;
 var obj = JSON.parse(Lancex-A);
 
 if($request.url.indexOf("/api/vip/v1/my") != -1 {
-}
+  obj."is_bought" : true,
+	obj."is_lifetime_vip" : true,
+	obj."is_public_beta_trial_vip" : true
+ }
 Lancex-A = JSON.stringify(obj);
-$done(name1908);
+$done(Lancex-A);
