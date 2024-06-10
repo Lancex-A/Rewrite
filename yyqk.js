d54271aaeea574c77c97e1e2a71adf1f15f5dbaf
wuming <132824538+Lancex-A@users.noreply.github.com>
2024-06-10 18:30:37 +0800

    Update yyqk.js

--- yyqk.js
+++ yyqk.js
@@ -1,27 +1,21 @@
 /*************************************
 
-项目名称：yyqk
+项目名称：
 下载地址：
-脚本作者：
+脚本作者：Lancex-A
 电报频道：
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 *************************************
 [rewrite_local]
-^https://api.yuceshu.cn:8443/api/Account/GetUserInfo? url script-response-body https://raw.githubusercontent.com/name1908/Rewrite/main/yyqk.js
+^ url script-response-body
 [mitm]
-hostname = api.yuceshu.cn
+hostname = 
 *************************************
-var name1908 = $response.body;
-var obj = JSON.parse(name1908);
+var Lancex-A = $response.body;
+var obj = JSON.parse(Lancex-A);
 
 if($request.url.indexOf("/api/Account/GetUserInfo") != -1 {
-    obj.isVip = true,
-    obj.data.nickName = "name1908";
-    obj.data = "2025-06-10 14:04:21";
-    obj.memberLevel = 11;
-    obj.vipValidTime = "2025-06-10 14:04:21";
 }
-
-name1908 = JSON.stringify(obj);
+Lancex-A = JSON.stringify(obj);
 $done(name1908);
