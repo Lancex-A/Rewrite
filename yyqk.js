23d51f36e05712cc6324550464bd361b35a75be2
wuming <132824538+name1908@users.noreply.github.com>
2024-06-10 14:18:49 +0800

    Update yyqk.js

--- yyqk.js
+++ yyqk.js
@@ -8,4 +8,4 @@
 
 *************************************
 [rewrite_local]
-^
+^https://api.yuceshu.cn:8443/api/Account/GetUserInfo?platform=101&version=2.1.8&city=1&device=8348CAB6-3922-486D-8D61-C1D427DEFA85&channel=&source=0 url script-response-body
